5b17628459f0489af65e1832ebfbe799065c5924
Sjoerd Siebinga <sjoerd.siebinga@gmail.com>
2010-02-23 10:53:16 +0100

    updated some imports in module integration tests

--- integration-tests/src/test/java/eu/europeana/frontend/FrontendTestUtil.java
+++ integration-tests/src/test/java/eu/europeana/frontend/FrontendTestUtil.java
@@ -26,13 +26,14 @@ import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import com.gargoylesoftware.htmlunit.html.HtmlPasswordInput;
 import com.gargoylesoftware.htmlunit.html.HtmlSubmitInput;
 import com.gargoylesoftware.htmlunit.html.HtmlTextInput;
-
 import eu.europeana.bootstrap.PortalFullStarter;
 import eu.europeana.bootstrap.SolrStarter;
 import eu.europeana.core.database.UserDao;
 import eu.europeana.core.database.domain.Role;
 import eu.europeana.core.database.domain.User;
 import eu.europeana.core.database.incoming.cli.ContentLoader;
+import org.junit.Test;
+import org.mortbay.jetty.Server;
 
 import java.io.IOException;
 import java.util.Date;
