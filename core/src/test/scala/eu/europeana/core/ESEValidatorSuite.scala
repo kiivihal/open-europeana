8abee8e9a8536b0fd0bcfe07376f6c5331a73d47
Sjoerd Siebinga <sjoerd.siebinga@gmail.com>
2010-03-10 14:31:25 +0100

    more scalatest classes added

--- core/src/test/scala/eu/europeana/core/ESEValidatorSuite.scala
+++ core/src/test/scala/eu/europeana/core/ESEValidatorSuite.scala
@@ -4,6 +4,7 @@ import org.scalatest.{FunSuite}
 import collection.mutable.Stack
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
+import org.scalatest.matchers.ShouldMatchers
 
 /**
  *
@@ -12,27 +13,24 @@ import org.scalatest.junit.JUnitRunner
  */
 
 @RunWith(classOf[JUnitRunner])
-class ESEValidatorSuite extends FunSuite {
+class ESEValidatorSuite extends FunSuite with ShouldMatchers{
 
   test("pop is invoked on a non-empty stack") {
 
-    val stack = new Stack[Int]
-    stack.push(1)
-    stack.push(2)
-    val oldSize = stack.size
-    val result = stack.pop()
-    assert(result === 2)
-    assert(stack.size === oldSize - 1)
-  }
-
-  test("pop is invoked on an empty stack") {
-
-    val emptyStack = new Stack[String]
-    intercept[NoSuchElementException] {
-      emptyStack.pop()
+      val stack = new Stack[Int]
+      stack.push(1)
+      stack.push(2)
+      val oldSize = stack.size
+      val result = stack.pop()
+      result should equal (2)
+      stack.size should equal (oldSize - 1)
     }
-    assert(emptyStack.isEmpty)
-  }
 
+    test("pop is invoked on an empty stack") {
+
+      val emptyStack = new Stack[String]
+      evaluating { emptyStack.pop() } should produce [NoSuchElementException]
+      emptyStack should be ('empty)
+    }
 
 }
